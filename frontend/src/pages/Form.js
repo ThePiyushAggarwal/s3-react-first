299bf5032abf8041a78ee6819e17ed4b413ef8a1
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-06-15 15:27:28 +0530

    drag and drop working react-dropzone

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -1,6 +1,38 @@
+import { useMemo } from 'react'
 import axios from 'axios'
 import { gql, useQuery } from '@apollo/client'
+import { useDropzone } from 'react-dropzone'
 
+// Styles for Drap and Drop component
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out',
+}
+
+const focusedStyle = {
+  borderColor: '#2196f3',
+}
+
+const acceptStyle = {
+  borderColor: '#00e676',
+}
+
+const rejectStyle = {
+  borderColor: '#ff1744',
+}
+
+// GraphQL Query
 const GET_IMAGE = gql`
   query {
     getAllUser {
@@ -9,18 +41,35 @@ const GET_IMAGE = gql`
   }
 `
 
-function Form() {
+function Form(props) {
   const { data, refetch } = useQuery(GET_IMAGE)
+  const {
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+    acceptedFiles,
+  } = useDropzone({ accept: { 'image/*': [] } })
 
-  const onSubmit = (e) => {
-    e.preventDefault()
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  )
+
+  const onSubmit = () => {
     const config = {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
     }
 
-    const data = Array.from(e.target[0].files)
+    const data = acceptedFiles
 
     console.log(data)
 
@@ -37,10 +86,13 @@ function Form() {
 
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <input type='file' accept='image/png, image/jpeg' multiple />
-        <button type='submit'>Submit File</button>
-      </form>
+      <div className='container'>
+        <div {...getRootProps({ style })}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+        <button onClick={onSubmit}>Submit</button>
+      </div>
 
       {data &&
         data?.getAllUser[0]?.imageUrls.map((image) => (
